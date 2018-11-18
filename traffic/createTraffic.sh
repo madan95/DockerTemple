04e75c86cfc859e02672152eb048840d2b8a45d1
madan <madan@vps444173.ovh.net>
2018-11-18 15:57:03 +0100

    k

--- traffic/createTraffic.sh
+++ traffic/createTraffic.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#Check if acme.json exist
+if [ ! -f  acme.json ]; then
+	echo 'acme.json not found!';
+	touch acme.json
+fi
+
 # Holds the key for Letencrypt SSL 
 chmod 600 acme.json
 
@@ -8,4 +14,4 @@ if [ -z "$(docker network ls | grep -F -i traffic)" ]; then
   docker network create traffic
 fi
 
-docker-compose up -d
+docker-compose up -d --build
